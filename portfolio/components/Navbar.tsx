bac70b9391e5176d767fff59910e56cfe3c70109
Julia Nguyen <juliatnguyen05@gmail.com>
2026-03-21 21:17:37 -0700

    feat: add navbar

---
+++ portfolio/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export default function Navbar(){
+    return (
+        <>
+        <div className="flex justify-between">
+            <p>Home</p>
+            <div className="flex gap-8">
+                <p>About</p>
+                <p>Experience</p>
+                <p>Projects</p>
+                <p>Design</p>
+                <p>Research</p>
+                <p>Resume</p>
+            </div>
+        </div>
+        </>
+    )
+}
